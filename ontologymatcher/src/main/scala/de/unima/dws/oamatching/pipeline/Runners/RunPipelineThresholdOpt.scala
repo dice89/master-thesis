6f28cf8f583686255a09cae643b8bd45d6faeffa
Alexander Mueller <alexan.chr.mueller@gmail.com>
2015-02-07 21:29:30 +0100

    added init to large optimise

--- ontologymatcher/src/main/scala/de/unima/dws/oamatching/pipeline/Runners/RunPipelineThresholdOpt.scala
+++ ontologymatcher/src/main/scala/de/unima/dws/oamatching/pipeline/Runners/RunPipelineThresholdOpt.scala
@@ -1,5 +1,6 @@
 package de.unima.dws.oamatching.pipeline.Runners
 
+import de.unima.dws.oamatching.matcher.MatcherRegistry
 import de.unima.dws.oamatching.pipeline.evaluation.EvaluationMatchingRunner
 import de.unima.dws.oamatching.pipeline.optimize.ParameterOptimizer
 
@@ -7,6 +8,7 @@ import de.unima.dws.oamatching.pipeline.optimize.ParameterOptimizer
  * Created by mueller on 07/02/15.
  */
 object RunPipelineThresholdOpt extends App{
+  MatcherRegistry.initLargeScale()
   val problems = EvaluationMatchingRunner.parseConference("ontos/2014/conference")
 
   ParameterOptimizer.optimizeAdvancedPipeline("conference",problems,ParameterOptimizer.getDoubleGrid(0.1,0.95,15))
