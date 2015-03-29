481d711d628a08b297cf6b1a8bec5b9ac9e4065d
Alexander Mueller <alexan.chr.mueller@gmail.com>
2015-03-29 19:20:47 +0200

    Added New Separated Threshold Opt

--- ontologymatcher/src/main/scala/de/unima/dws/oamatching/thesis/SeparatedOptimization.scala
+++ ontologymatcher/src/main/scala/de/unima/dws/oamatching/thesis/SeparatedOptimization.scala
@@ -25,7 +25,7 @@ trait SeparatedOptimization extends ResultServerHandling with LazyLogging with O
     val process_type: String = processes.get(process_name_with_ending).get
 
 
-    val normalized_per_category = ref_matching_pairs.par.map { case (ref_file, matching_file) => {
+    val normalized_per_category: List[(Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Alignment)] = ref_matching_pairs.par.map { case (ref_file, matching_file) => {
 
       val ref_alignment: Alignment = ref_file.reference
 
@@ -40,64 +40,23 @@ trait SeparatedOptimization extends ResultServerHandling with LazyLogging with O
       val dp_normalized = getNormalizedScores(dp_alignment, result.dp_matchings)
       val op_normalized = getNormalizedScores(op_alignment, result.op_matchings)
 
-      ((class_normalized, dp_normalized, op_normalized), (ref_alignment))
+      (class_normalized, dp_normalized, op_normalized, (ref_alignment))
     }
     }.toList
 
-    val tuple_wise: (List[Map[String, (Map[MatchRelation, Double], Alignment)]], List[Map[String, (Map[MatchRelation, Double], Alignment)]], List[Map[String, (Map[MatchRelation, Double], Alignment)]]) = normalized_per_category.unzip._1.unzip3
+
+    val optimization_grid = ParameterOptimizer.getDoubleGrid(0.001, 1.1, 1000)
+    val optimal_thresholds = findOptimalThresholds(selection_function, normalized_per_category, optimization_grid)
 
     logger.info(s"Start threshold optimization for $ds_name and $process_name in run $run_number")
 
-
-
-    val optimization_grid = ParameterOptimizer.getDoubleGrid(0.001, 1.1, 1000)
-    //globally normalize each tuple
-    val best_threshold_classes = findOptimalThresholdGlobalOnly(selection_function, tuple_wise._1, optimization_grid)
-    val best_threshold_dps = findOptimalThresholdGlobalOnly(selection_function, tuple_wise._2, optimization_grid)
-    val best_threshold_ops = findOptimalThresholdGlobalOnly(selection_function, tuple_wise._3, optimization_grid)
-
     //construct final matchings and evaluate
-    val best_results: Map[String, SeparatedResult] = best_threshold_classes.best_global_results.keys.map(norm_technique => {
-      val class_threshold = best_threshold_classes.best_global_results.get(norm_technique).get._1
-      val dp_threshold = best_threshold_dps.best_global_results.get(norm_technique).get._1
-      val op_threshold = best_threshold_ops.best_global_results.get(norm_technique).get._1
-
-
-      val eased_thresholds = List((class_threshold - 0.01, dp_threshold - 0.01, op_threshold - 0.01), (class_threshold - 0.025, dp_threshold - 0.025, op_threshold - 0.025), (class_threshold - 0.05, dp_threshold - 0.05, op_threshold - 0.05))
-      val eased_thresholds_results = eased_thresholds.map(triple => {
-
-        val results = normalized_per_category.map { case ((classes_matchings, dp_matchings, op_matchings), ref_align) => {
-
-          val all_matchings_init = classes_matchings ++ dp_matchings ++ op_matchings
-          val all_matchings = all_matchings_init.get(norm_technique).get._1
-
-          val selected_classes: Map[MatchRelation, Double] = selection_function(classes_matchings.get(norm_technique).get._1, triple._1)
-          val selected_dps: Map[MatchRelation, Double] = selection_function(dp_matchings.get(norm_technique).get._1, triple._2)
-          val selected_ops: Map[MatchRelation, Double] = selection_function(op_matchings.get(norm_technique).get._1, triple._3)
-
-          val all_selected_matchings_list = selected_classes ++ selected_dps ++ selected_ops
-
-          val eval_res = debugAndEvaluate(op_threshold, all_matchings, ref_align, all_selected_matchings_list, norm_technique,true)
-
-
-
-          eval_res
-        }
-        }.toList
-
-
-        val agg_res = EvaluationMatchingRunner.computeAggregatedResults(results)
-        println(s"Result for $triple")
-        println(agg_res)
-        norm_technique -> SeparatedResult( triple._1,  triple._2, triple._3, agg_res)
-
-      })
-
-
-      eased_thresholds_results.maxBy(_._2.result.macro_eval_res.f1Measure)
-    }).toMap
-
-
+    val best_results = optimal_thresholds.map { case (norm_technique, (class_threshold, dp_threshold, op_threshold)) => {
+      val eval_res = evaluateRound(norm_technique, selection_function, normalized_per_category, class_threshold, dp_threshold, op_threshold,true)
+      println(eval_res)
+      norm_technique -> eval_res
+    }
+    }
 
 
     val best_result = best_results.maxBy(res_by_norm => res_by_norm._2.result.macro_eval_res.f1Measure)
@@ -116,16 +75,139 @@ trait SeparatedOptimization extends ResultServerHandling with LazyLogging with O
    * @param test
    * @return
    */
-  def getNormalizedScores(ref_alignment: Alignment, test: (Int, Map[String, (Double, Double)], Map[MatchRelation, Double])): Map[String, (Map[MatchRelation, Double], Alignment)] = {
+  def getNormalizedScores(ref_alignment: Alignment, test: (Int, Map[String, (Double, Double)], Map[MatchRelation, Double])): Map[String, Map[MatchRelation, Double]] = {
     val norm_res_gaussian: Map[MatchRelation, Double] = ScoreNormalizationFunctions.normalizeByGaussianScaling(test._1, test._2, test._3).toMap
     val norm_res_euclidean_max: Map[MatchRelation, Double] = ScoreNormalizationFunctions.normalizeByMaxEuclideanDistance(test._1, test._2, test._3).toMap
     val norm_res_gamma: Map[MatchRelation, Double] = ScoreNormalizationFunctions.normalizeByGammaScaling(test._1, test._2, test._3).toMap
     val norm_res_znorm: Map[MatchRelation, Double] = ScoreNormalizationFunctions.normalizeByZScore(test._1, test._2, test._3).toMap
 
-    val resulting_matchings: Map[String, (Map[MatchRelation, Double], Alignment)] = Map(("none", (test._3, ref_alignment)), ("gaussian", (norm_res_gaussian, ref_alignment)), ("zscore", (norm_res_znorm, ref_alignment)), ("gammma", (norm_res_gamma, ref_alignment)), ("euclidean_max", (norm_res_euclidean_max, ref_alignment)))
+    val resulting_matchings: Map[String, Map[MatchRelation, Double]] = Map(("none", test._3), ("gaussian", norm_res_gaussian), ("zscore", norm_res_znorm), ("gammma", norm_res_gamma), ("euclidean_max", norm_res_euclidean_max))
 
     resulting_matchings
   }
+
+
+  /**
+   *
+   * @param selection_function
+   * @param class_matchings
+   * @param dp_matchings
+   * @param op_matchings
+   * @param class_threshold
+   * @param dp_threshold
+   * @param op_threshold
+   * @param ref_alignment
+   * @return
+   */
+  def selectAndEvaluate(selection_function: (Map[MatchRelation, Double], Double) => Map[MatchRelation, Double], class_matchings: Map[MatchRelation, Double], dp_matchings: Map[MatchRelation, Double], op_matchings: Map[MatchRelation, Double], class_threshold: Double, dp_threshold: Double, op_threshold: Double, ref_alignment: Alignment, debug: Boolean): EvaluationResult = {
+
+    val unselected = class_matchings ++ dp_matchings ++ op_matchings
+
+    val selected_classes: Map[MatchRelation, Double] = selection_function(class_matchings, class_threshold)
+    val selected_dps: Map[MatchRelation, Double] = selection_function(dp_matchings, dp_threshold)
+    val selected_ops: Map[MatchRelation, Double] = selection_function(op_matchings, op_threshold)
+
+    val selected: Map[MatchRelation, Double] = selected_classes ++ selected_dps ++ selected_ops
+
+    val result = if (debug) {
+      debugAndEvaluateSeparated(class_threshold, dp_threshold, op_threshold, unselected, ref_alignment, selected, "", false)
+
+    } else {
+      val alignment = new Alignment(null, null, selected)
+
+      alignment.evaluate(ref_alignment)
+    }
+
+    result
+  }
+
+
+  def evaluateRound(norm_technique: String, selection_function: (Map[MatchRelation, Double], Double) => Map[MatchRelation, Double], normalizedScores: List[(Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Alignment)], class_threshold: Double, dp_threshold: Double, op_threshold: Double, debug: Boolean): SeparatedResult = {
+
+    val unique_techniques = normalizedScores.head._1.keys.toVector
+
+    //get single results
+    val results: List[EvaluationResult] = normalizedScores.map { case (class_matchings, dp_matchings, op_matchings, ref_alignment) => {
+
+      val class_matchings_norm = class_matchings.get(norm_technique).get
+      val dp_matchings_norm = dp_matchings.get(norm_technique).get
+      val op_matchings_norm = op_matchings.get(norm_technique).get
+
+      selectAndEvaluate(selection_function, class_matchings_norm, dp_matchings_norm, op_matchings_norm, class_threshold, dp_threshold, op_threshold, ref_alignment, debug)
+
+    }
+    }.toList
+
+    //get result list by norm
+
+    val agg_res = EvaluationMatchingRunner.computeAggregatedResults(results)
+    SeparatedResult(class_threshold, dp_threshold, op_threshold, agg_res)
+
+  }
+
+
+  /**
+   *
+   * @param selection_function
+   * @param normalizedScores
+   * @param threshold_grid
+   * @return
+   */
+  def findOptimalThresholds(selection_function: (Map[MatchRelation, Double], Double) => Map[MatchRelation, Double], normalizedScores: List[(Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Map[String, Map[MatchRelation, Double]], Alignment)], threshold_grid: List[Double]): Map[String, (Double, Double, Double)] = {
+
+    val unique_techniques = normalizedScores.head._2.keys.toVector
+    //optimize thresholds
+
+
+    //first find best class threshold
+
+    val best_class_thresholds_by_norm: Map[String, Double] = unique_techniques.map(norm_technique => {
+      val results_by_threshold = threshold_grid.map(threshold => {
+        (threshold, evaluateRound(norm_technique, selection_function, normalizedScores, threshold, 1.1, 1.1, false))
+      })
+      val best_result = results_by_threshold.maxBy(_._2.result.macro_eval_res.f1Measure)
+
+      norm_technique -> best_result._1
+    }).toMap
+
+    //find best dp threshold
+    val best_dp_thresholds_by_norm: Map[String, Double] = unique_techniques.map(norm_technique => {
+      val class_threshold = best_class_thresholds_by_norm.get(norm_technique).get
+      val results_by_threshold = threshold_grid.map(threshold => {
+
+        (threshold, evaluateRound(norm_technique, selection_function, normalizedScores, class_threshold, threshold, 1.1, false))
+      })
+      val best_result = results_by_threshold.maxBy(_._2.result.macro_eval_res.f1Measure)
+
+      norm_technique -> best_result._1
+    }).toMap
+
+
+    val best_op_thresholds_by_norm: Map[String, Double] = unique_techniques.map(norm_technique => {
+      val class_threshold = best_class_thresholds_by_norm.get(norm_technique).get
+      val dp_threshold = best_dp_thresholds_by_norm.get(norm_technique).get
+      val results_by_threshold = threshold_grid.map(threshold => {
+
+        (threshold, evaluateRound(norm_technique, selection_function, normalizedScores, class_threshold, dp_threshold, threshold, false))
+      })
+      val best_result = results_by_threshold.maxBy(_._2.result.macro_eval_res.f1Measure)
+
+      norm_technique -> best_result._1
+    }).toMap
+
+
+    //get best thresholds by norm technique
+    val best_by_norm_technique: Map[String, (Double, Double, Double)] = unique_techniques.map(norm_technique => {
+      val best_class = best_class_thresholds_by_norm.get(norm_technique).get
+      val best_dp = best_dp_thresholds_by_norm.get(norm_technique).get
+      val best_op = best_op_thresholds_by_norm.get(norm_technique).get
+
+      norm_technique ->(best_class, best_dp, best_op)
+    }).toMap
+
+    best_by_norm_technique
+  }
+
 
   /**
    *
